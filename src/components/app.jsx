f8e2d925d76ec8aa79dc387fe6e7d2ad8c65ebd6
houzairkoussa <houzairmk@icloud.com>
2021-12-09 17:48:36 +0400

    renders html structure

---
+++ src/components/app.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  // constructor(props){
+  //   super(props);
+  // }
+
+  render() {
+    return(
+      <div>
+        <div className='left-scene'>
+          <div className='form-search'>
+
+          </div>
+          <div className="selected-gif">
+
+          </div>
+        </div>
+        <div className='right-scene'>
+          <div className="gif-list">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
